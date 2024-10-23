556a776a187d835828f3bdaeee7fd70855aadfbd
Ahmed Himel <ahmedhimel000@gmail.com>
2024-10-23 12:11:58 +0600

    remove unused directory

--- dist/app/modules/blog/blog.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BlogRoutes = void 0;
-const express_1 = require("express");
-const blog_controller_1 = require("./blog.controller");
-const router = (0, express_1.Router)();
-router.post('/create-blog', blog_controller_1.BlogController.createBlog);
-router.get('/', blog_controller_1.BlogController.getBlog);
-exports.BlogRoutes = router;
--- dist/app/modules/skills/skills.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SkillRoutes = void 0;
-const express_1 = require("express");
-const skills_controller_1 = require("./skills.controller");
-const router = (0, express_1.Router)();
-router.post('/create-skill', skills_controller_1.SkillController.createSkill);
-router.get('/', skills_controller_1.SkillController.getSkill);
-exports.SkillRoutes = router;
